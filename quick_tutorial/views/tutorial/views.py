b4d71a255a7658833aa9c8153fda135c08229351
Corey Leveen <mntj11@gmail.com>
2015-06-12 11:11:17 -0400

    Update tests. Move VENV

--- quick_tutorial/views/tutorial/views.py
+++ quick_tutorial/views/tutorial/views.py
@@ -2,7 +2,7 @@ from pyramid.response import Response
 from pyramid.view import view_config
 
 
-@view_config(route_name='name')
+@view_config(route_name='home')
 def home(request):
     return Response('<body>Visit <a href="/howdy">hello</a></body')
 
