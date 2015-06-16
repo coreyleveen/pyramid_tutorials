102150121f595b894589c0bf21bcb38e4eef91b6
Corey Leveen <mntj11@gmail.com>
2015-06-16 18:28:56 -0400

    Update tests

--- quick_tutorial/request_response/tutorial/tests.py
+++ quick_tutorial/request_response/tutorial/tests.py
@@ -16,29 +16,39 @@ class TutorialViewTests(unittest.TestCase):
         request = testing.DummyRequest()
         inst = TutorialViews(request)
         response = inst.home()
-        self.assertEqual('Home View', response['name'])
+        self.assertEqual(response.status, '302 Found')
 
-    def test_hello(self):
+    def test_plain_without_name(self):
         from .views import TutorialViews
 
         request = testing.DummyRequest()
         inst = TutorialViews(request)
-        response = inst.hello()
-        self.assertEqual('Hello View', response['name'])
+        response = inst.plain()
+        self.assertIn(b'No Name Provided', response.body)
+
+    def test_plain_with_name(self):
+        from .views import TutorialViews
+
+        request = testing.DummyRequest()
+        request.GET['name'] = 'Jane Doe'
+        inst = TutorialViews(request)
+        response = inst.plain()
+        self.assertIn(b'Jane Doe', response.body)
 
 
 class TutorialFunctionalTests(unittest.TestCase):
     def setUp(self):
         from tutorial import main
+
         app = main({})
         from webtest import TestApp
 
         self.testapp = TestApp(app)
 
-    def test_home(self):
-        res = self.testapp.get('/', status=200)
-        self.assertIn(b'<h1>Hi Home View', res.body)
+    def test_plain_without_name(self):
+        res = self.testapp.get('/plain', status=200)
+        self.assertIn(b'No Name Provided', res.body)
 
-    def test_hello(self):
-        res = self.testapp.get('/howdy', status=200)
-        self.assertIn(b'<h1>Hi Hello View', res.body)
+    def test_plain_with_name(self):
+        res = self.testapp.get('/plain?name=Jane%20Doe', status=200)
+        self.assertIn(b'Jane Doe', res.body)
